11f0e52d87c65201d9a85df810a84345fb20930c
Elijah <elijah.ivanik@inbox.ru>
2023-01-29 10:51:54 +0300

    feat: design pattern Factory

---
+++ design_patterns/factory.js
@@ -0,0 +1,39 @@
+/*
+ * это Класс (супер-класс), который будет создавать
+ * определенные Объекты на основании входных данных
+ 
+ ! когда нужно создавать множество однотипных Объектов:
+ ! объектов с одинаковой структурой, но разными данными
+
+ ? применение: когда нам нужно создать сложный объект из нескольких источников
+ ? или создать несколько объектов с одинаковой структурой, но разными данными
+*/
+
+// * constructor
+class Bmw {
+  constructor(type, price, maxSpeed) {
+    this.type = type;
+    this.price = price;
+    this.maxSpeed = maxSpeed;
+  }
+}
+
+// * factory
+class BmwFactory {
+  create(type) {
+    switch (type) {
+      case "X5":
+        return new Bmw(type, 110000, 220);
+      case "X6":
+        return new Bmw(type, 150000, 300);
+    }
+  }
+}
+const factory = new BmwFactory();
+
+// * implementation
+const x5 = factory.create("X5");
+const x6 = factory.create("X6");
+
+// ! при большом количестве создаваемых объектов шаблон фабрики начнет разрастаться
+// ! это решается с помощью Абстрактной Фабрики
